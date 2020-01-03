6086abd778250c7a01ecc940cc49d98ab15834b6
Giu Roperto <giu.roperto@gmail.com>
2020-01-03 10:06:28 -0300

    all backend done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      res.render('movies', { movies });
+    })
+    .catch(err => console.error('Error finding movies in the database', err))
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movie => {
+      res.render('detailed-movie', { movie });
+    })
+    .catch(err => console.error('Error finding movies in the database', err))
 });
 
 module.exports = router;
